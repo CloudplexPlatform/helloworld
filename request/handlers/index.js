22e9092624dd65b4d5c7adf5b08e7e87544a5dbb
Haseeb Humayun <haseeb.h002@gmail.com>
2019-12-18 15:22:08 +0500

    remove mysql dependency

--- request/handlers/index.js
+++ request/handlers/index.js
@@ -1,14 +1,18 @@
-const { mysql } = require('../../db/mysql/mysql');
+//const { mysql } = require('../../db/mysql/mysql');
 
 const showIndex = async (req, res) => {
-    mysql.query('SELECT text from dummy', function (error, results) {
-        if (error) throw error;
-        else if( results ) {
-            res.render('index', {
-                text: results[0].text
+     res.render('index', {
+                //text: results[0].text
+         text: "Hello World"
             });
-        }
-    });
+//     mysql.query('SELECT text from dummy', function (error, results) {
+//         if (error) throw error;
+//         else if( results ) {
+//             res.render('index', {
+//                 text: results[0].text
+//             });
+//         }
+//     });
 };
 
 
